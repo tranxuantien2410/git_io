ed97ff015885bebb2783c198f25ca8a6a72004f4
tranxuantien2410 <tient5373@gmail.com>
2024-01-21 23:32:13 +0700

    first commit

---
+++ Bai1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+unsigned long long tinhGiaiThua(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * tinhGiaiThua(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Nhap so nguyen duong n: ";
+    std::cin >> n;
+    if (n < 0) {
+        std::cout << "Vui long nhap so nguyen duong.\n";
+    } else {
+        unsigned long long ketQua = tinhGiaiThua(n);
+        std::cout << "Giai thua cua " << n << " la: " << ketQua << "\n";
+    }
+
+    return 0;
+}
